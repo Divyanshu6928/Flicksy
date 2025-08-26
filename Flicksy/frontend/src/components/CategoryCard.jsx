a39021a007069daaf972b7da60e629b8d34aaae0
Divyanshu <lyricalv15@gmail.com>
2025-08-26 22:49:37 +0530

    Category-section done

--- Flicksy/frontend/src/components/CategoryCard.jsx
+++ Flicksy/frontend/src/components/CategoryCard.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
-import '../styles/CategoryCard.css'
+import '../styles/CategoryCard.css';
+
+const cardname = [
+  "FlickBox",
+  "ExamPick",
+  "Emergency Kit",
+  "Party Pack",
+  "Instant Foods",
+  "Gifts & Surprises",
+  "Essentials",
+  "Electronics"
+];
 
 const CategoryCard = () => {
   return (
-    <div>
-        
+    <div className="category-container">
+      {cardname.map((name, index) => (
+        <div key={index} className="category-card">
+          {name}
+        </div>
+      ))}      
     </div>
-  )
-}
+  );
+};
 
 export default CategoryCard;
--- Flicksy/frontend/src/pages/Home.jsx
+++ Flicksy/frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Header from '../components/Header';
 import SearchBar from '../components/SearchBar';
 import HeroSection from '../components/HeroSection';
+import CategoryCard from '../components/CategoryCard';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
         <Header/>
         <SearchBar/>
         <HeroSection/>
+        <CategoryCard/>
     </div>
   )
 }
