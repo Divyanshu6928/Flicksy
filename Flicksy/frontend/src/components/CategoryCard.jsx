b59533c4eccf6c27c9b6fdc3520fc676d88f96db
Divyanshu <lyricalv15@gmail.com>
2025-08-26 21:51:58 +0530

    Hero-section done

---
+++ Flicksy/frontend/src/components/CategoryCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/CategoryCard.css'
+
+const CategoryCard = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default CategoryCard;
--- Flicksy/frontend/src/components/HeroSection.jsx
+++ Flicksy/frontend/src/components/HeroSection.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import "../styles/HeroSection.css";
 
 const HeroSection = () => {
   return (
-    <div>HeroSection</div>
-  )
-}
+    <div className="main-hero">
+      <div className="hero-section m-5">
+        <div className="center-box">
+          <h1>Hostel Adda for Food & Essentials</h1>
+          <div className="btn-container">
+            <button className=" btnBrowse">Browse Products <span className="bi bi-cart-fill"> </span></button>
+            <button className=" btnEmergency">Emergency Kit !! <span className="bi bi-brightness-alt-high"> </span> </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default HeroSection;
